528dbfef0bd4a6cb69bdc594ac7513a4c8288b1e
Your Name <you@example.com>
2025-03-17 13:10:51 +0530

    updated

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -5,43 +5,53 @@ provider "google" {
 
 data "google_client_config" "default" {}
 
-data "google_container_cluster" "gke" {
-  name     = google_container_cluster.gke.name
-  location = google_container_cluster.gke.location
-}
-
 resource "google_container_cluster" "gke" {
   name     = "gke-cluster"
   location = "us-central1"
-  initial_node_count = 1
+  initial_node_count = 2
   node_config {
-    machine_type = "e2-micro"
-    disk_size_gb = 10
+    machine_type = "e2-medium"
   }
 }
 
 provider "kubernetes" {
-  host                   = "https://${data.google_container_cluster.gke.endpoint}"
+  host                   = google_container_cluster.gke.endpoint
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.gke.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(google_container_cluster.gke.master_auth.0.cluster_ca_certificate)
 }
 
-resource "kubernetes_namespace" "postgres" {
+resource "kubernetes_config_map" "postgres_config" {
+  metadata {
+    name = "postgres-config"
+  }
+  data = {
+    POSTGRES_DB       = "corda_db"
+    POSTGRES_USER     = "corda_user"
+    POSTGRES_PASSWORD = "corda_password"
+  }
+}
+
+resource "kubernetes_persistent_volume_claim" "postgres_pvc" {
+  metadata {
+    name = "postgres-pvc"
+  }
+  spec {
+    access_modes = ["ReadWriteOnce"]
+    resources {
+      requests = {
+        storage = "10Gi"
+      }
+    }
+  }
+}
+
+resource "kubernetes_stateful_set" "postgres" {
   metadata {
     name = "postgres"
   }
-}
-
-resource "kubernetes_deployment" "postgres" {
-  metadata {
-    name      = "postgres"
-    namespace = kubernetes_namespace.postgres.metadata[0].name
-    labels = {
-      app = "postgres"
-    }
-  }
   spec {
-    replicas = 1
+    service_name = "postgres"
+    replicas     = 1
     selector {
       match_labels = {
         app = "postgres"
@@ -60,26 +70,32 @@ resource "kubernetes_deployment" "postgres" {
           port {
             container_port = 5432
           }
-          env {
-            name  = "POSTGRES_DB"
-            value = "corda_db"
-          }
-          env {
-            name  = "POSTGRES_USER"
-            value = "corda_user"
-          }
-          env {
-            name  = "POSTGRES_PASSWORD"
-            value = "corda_password"
+          env_from {
+            config_map_ref {
+              name = kubernetes_config_map.postgres_config.metadata[0].name
+            }
           }
           volume_mount {
             mount_path = "/var/lib/postgresql/data"
             name       = "postgres-storage"
+            sub_path   = "postgres"
+          }
+          resources {
+            requests = {
+              cpu    = "500m"
+              memory = "512Mi"
+            }
+            limits = {
+              cpu    = "1000m"
+              memory = "1Gi"
+            }
           }
         }
         volume {
           name = "postgres-storage"
-          empty_dir {}
+          persistent_volume_claim {
+            claim_name = kubernetes_persistent_volume_claim.postgres_pvc.metadata[0].name
+          }
         }
       }
     }
@@ -88,8 +104,7 @@ resource "kubernetes_deployment" "postgres" {
 
 resource "kubernetes_service" "postgres" {
   metadata {
-    name      = "postgres"
-    namespace = kubernetes_namespace.postgres.metadata[0].name
+    name = "postgres"
   }
   spec {
     selector = {
@@ -100,5 +115,40 @@ resource "kubernetes_service" "postgres" {
       target_port = 5432
     }
     type = "ClusterIP"
+  }
+}
+
+resource "kubernetes_horizontal_pod_autoscaler" "postgres_hpa" {
+  metadata {
+    name = "postgres-hpa"
+  }
+  spec {
+    scale_target_ref {
+      kind = "StatefulSet"
+      name = kubernetes_stateful_set.postgres.metadata[0].name
+      api_version = "apps/v1"
+    }
+    min_replicas = 1
+    max_replicas = 3
+    metric {
+      type = "Resource"
+      resource {
+        name  = "cpu"
+        target {
+          type                = "Utilization"
+          average_utilization = 70
+        }
+      }
+    }
+    metric {
+      type = "Resource"
+      resource {
+        name  = "memory"
+        target {
+          type                = "Utilization"
+          average_utilization = 75
+        }
+      }
+    }
   }
 }
